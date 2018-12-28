43fec7f2cf33f8d22e5a5ab8348fab6a33bded7f
xiangjiaoren <wangyunfeng@126.com>
2018-12-28 16:11:02 +0800

    treadlocaltest

---
+++ src/readcode/testThreadLocal.java
@@ -0,0 +1,122 @@
+package readcode;
+
+//testThreadLocal
+
+public class testThreadLocal {
+	
+	
+	/**
+	 * ThreadLocal用法
+	 * @author coshaho
+	 *
+	 */
+
+	    private static final ThreadLocal<Object> threadLocal = new ThreadLocal<Object>(){
+	        /**
+	         * ThreadLocal没有被当前线程赋值时或当前线程刚调用remove方法后调用get方法，返回此方法值
+	         */
+	        @Override
+	        protected Object initialValue()
+	        {
+	            System.out.println("调用get方法时，当前线程共享变量没有设置，调用initialValue获取默认值！");
+	            return null;
+	        }
+	    };
+	     
+	    public static void main(String[] args)
+	    {
+	        new Thread(new MyIntegerTask("IntegerTask1")).start();
+	      //  new Thread(new MyStringTask("StringTask1")).start();
+	    //   new Thread(new MyIntegerTask("IntegerTask2")).start();
+	      //  new Thread(new MyStringTask("StringTask2")).start();
+	    }
+	     
+	    public static class MyIntegerTask implements Runnable
+	    {
+	        private String name;
+	         
+	        MyIntegerTask(String name)
+	        {
+	            this.name = name;
+	        }
+	 
+	        @Override
+	        public void run()
+	        {
+	            for(int i = 0; i < 5; i++)
+	            {
+	                // ThreadLocal.get方法获取线程变量
+	                if(null == testThreadLocal.threadLocal.get())
+	                {
+	                    // ThreadLocal.et方法设置线程变量
+	                    testThreadLocal.threadLocal.set(0);
+	                    System.out.println("线程" + name + ": 0");
+	                }
+	                else
+	                {
+	                    int num = (Integer)testThreadLocal.threadLocal.get();
+	                    testThreadLocal.threadLocal.set(num + 1);
+	                    System.out.println("线程" + name + ": " + testThreadLocal.threadLocal.get());
+	                    if(i == 3)
+	                    {
+	                        testThreadLocal.threadLocal.remove();
+	                    }
+	                }
+	                try
+	                {
+	                    Thread.sleep(1000);
+	                }
+	                catch (InterruptedException e)
+	                {
+	                    e.printStackTrace();
+	                }
+	            }  
+	        }
+	         
+	    }
+	     
+	    public static class MyStringTask implements Runnable
+	    {
+	        private String name;
+	         
+	        MyStringTask(String name)
+	        {
+	            this.name = name;
+	        }
+	 
+	        @Override
+	        public void run()
+	        {
+	            for(int i = 0; i < 5; i++)
+	            {
+	                if(null == testThreadLocal.threadLocal.get())
+	                {
+	                    testThreadLocal.threadLocal.set("a");
+	                    System.out.println("线程" + name + ": a");
+	                }
+	                else
+	                {
+	                    String str = (String)testThreadLocal.threadLocal.get();
+	                    testThreadLocal.threadLocal.set(str + "a");
+	                    System.out.println("线程" + name + ": " + testThreadLocal.threadLocal.get());
+	                }
+	                try
+	                {
+	                    Thread.sleep(800);
+	                }
+	                catch (InterruptedException e)
+	                {
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+	         
+	    
+	    }
+
+	    }
+
+
+/////threadlocal深入连接
+
+
---
+++ src/readcode/testThreadLocal1.java
@@ -0,0 +1,59 @@
+package readcode;
+
+//testThreadLocal1
+
+public class testThreadLocal1 {
+
+	
+	 ThreadLocal<Long> longLocal = new ThreadLocal<Long>();
+	    ThreadLocal<String> stringLocal = new ThreadLocal<String>();
+	 
+	    
+/*	    private static final ThreadLocal<Long> longLocal = new ThreadLocal<Long>(){
+	        *//**
+	         * ThreadLocal没有被当前线程赋值时或当前线程刚调用remove方法后调用get方法，返回此方法值
+	         *//*
+	        @Override
+	        protected Object initialValue()
+	        {
+	            System.out.println("调用get方法时，当前线程共享变量没有设置，调用initialValue获取默认值！");
+	            return null;
+	        }
+	    };	    */
+	    
+	    
+	    
+	    public void set() {
+	        longLocal.set(Thread.currentThread().getId());
+	        stringLocal.set(Thread.currentThread().getName());
+	    }
+	     
+	    public long getLong() {
+	        return longLocal.get();
+	    }
+	     
+	    public String getString() {
+	        return stringLocal.get();
+	    }
+	     
+	    public static void main(String[] args) throws InterruptedException {
+	        final testThreadLocal1 test = new testThreadLocal1();
+	        test.set();
+	        System.out.println(test.getLong());
+	        System.out.println(test.getString());
+	 
+	        Thread thread1 = new Thread(){
+	            public void run() {
+	                test.set();
+	                System.out.println(test.getLong());
+	                System.out.println(test.getString());
+	            };
+	        };
+	        thread1.start();
+	        thread1.join();
+	         
+	        System.out.println(test.getLong());
+	        System.out.println(test.getString());
+	    }
+	
+}
